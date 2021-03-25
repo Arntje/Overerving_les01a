5312f12f76944d82d0a61226b842f7ca7924b3ae
Arntje <a.braakenburg@chello.nl>
2021-03-25 16:06:39 +0100

    poging-1

--- src/nl/novi/javaprogrammeren/overerving/SpaceXRocket.java
+++ src/nl/novi/javaprogrammeren/overerving/SpaceXRocket.java
@@ -2,8 +2,14 @@ package nl.novi.javaprogrammeren.overerving;
 
 public class SpaceXRocket extends Rocket {
 
+    @Override
+    public int calculateMetersFlown(int fuel) {
+        return fuel * 250;
+
+    }
     // We roepen hier de constructor aan van Rocket
     public SpaceXRocket(int maximumFuel) {
         super(maximumFuel);
     }
+
 }
